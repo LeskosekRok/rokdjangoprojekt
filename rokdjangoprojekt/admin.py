64b4c0ca5831930593f6bd062dd495338496d5c3
LeskosekRok <rok.leskosek1@gmail.com>
2026-02-16 16:38:40 +0100

    Popravil kodo

--- rokdjangoprojekt/admin.py
+++ rokdjangoprojekt/admin.py
@@ -1,68 +1,51 @@
-﻿# -*- coding: utf-8 -*-
-from django.contrib import admin, messages
+# -*- coding: utf-8 -*-
+from django.contrib import admin
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
+
 from .models import Ucitelj
 from django.contrib.auth.models import User
-import traceback
 
-@admin.action(description="Pošlji e-mail za registracijo in reset gesla")
+@admin.action(description="Poslji e-mail za registracijo in reset gesla")
 def send_registration_emails(modeladmin, request, queryset):
     for ucitelj in queryset:
+        # Poisci uporabnika glede na e-mail
         try:
-            email = (ucitelj.eposta or "").strip()
-            first_name = (ucitelj.ime or "").strip()
+            user = User.objects.get(email=ucitelj.eposta)
+        except User.DoesNotExist:
+            # Ce ga ni, ga ustvari
+            user = User.objects.create_user(
+                username=ucitelj.eposta,
+                email=ucitelj.eposta,
+                first_name=ucitelj.ime,
+                password=User.objects.make_random_password()
+            )
 
-            if not email:
-                messages.error(request, f"Ucitelj '{first_name}' nima e-pošte.")
-                continue
+        # Generiraj token za ponastavitev gesla
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
 
-            # Try to find existing user
-            user_qs = User.objects.filter(email=email)
-            if user_qs.exists():
-                user = user_qs.first()
-            else:
-                # Ensure username is non-empty and unique
-                username = email if email else User.objects.make_random_password()
-                while User.objects.filter(username=username).exists():
-                    username = User.objects.make_random_password()
-                
-                user = User.objects.create_user(
-                    username=username,
-                    email=email,
-                    first_name=first_name,
-                    password=User.objects.make_random_password()
-                )
+        # Link za ponastavitev gesla
+        reset_link = request.build_absolute_uri(
+            reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})
+        )
 
-            # Generate token for password reset
-            token = default_token_generator.make_token(user)
-            uid = urlsafe_base64_encode(force_bytes(user.pk))
+        # Link za registracijo
+        registration_link = f"https://django.fbeu.eu/welcome?email={user.email}"
 
-            # Build reset and registration links
-            reset_link = f"https://django.fbeu.eu/reset/{uid}/{token}/"
-            registration_link = f"https://django.fbeu.eu/welcome?email={user.email}"
-
-            # Send email safely
-            try:
-                send_mail(
-                    subject="Registracija vašega računa",
-                    message=f"Pozdravljeni {user.first_name},\n\n"
-                            f"Prosimo obiščite {registration_link}, da vidite svoj račun.\n"
-                            f"Za nastavitev gesla, kliknite sem: {reset_link}\n\n"
-                            f"Hvala!",
-                    from_email="admin@django.fbeu.eu",
-                    recipient_list=[user.email],
-                    fail_silently=False
-                )
-                messages.success(request, f"E-mail poslan uporabniku '{user.email}'.")
-            except Exception as email_error:
-                messages.error(request, f"Napaka pri pošiljanju e-maila '{user.email}': {email_error}")
-
-        except Exception as e:
-            messages.error(request, f"Napaka pri obdelavi '{ucitelj.ime}': {traceback.format_exc()}")
+        # Poslji e-mail
+        send_mail(
+            subject="Registracija vasega racuna",
+            message=f"Pozdravljeni {user.first_name},\n\n"
+                    f"Prosimo obiscite {registration_link}, da vidite svoj racun.\n"
+                    f"Za nastavitev gesla, kliknite sem: {reset_link}\n\n"
+                    f"Hvala!",
+            from_email="admin@django.fbeu.eu",
+            recipient_list=[user.email],
+        )
 
 class UciteljAdmin(admin.ModelAdmin):
     list_display = ('ime', 'eposta')
