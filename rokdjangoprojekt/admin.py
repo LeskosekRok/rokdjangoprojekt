217b20a30bca421abd61350b87329f6b1b744115
LeskosekRok <rok.leskosek1@gmail.com>
2026-02-16 19:21:21 +0100

    Popravil posiljanje emaila

--- rokdjangoprojekt/admin.py
+++ rokdjangoprojekt/admin.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.core.mail import send_mail
+import smtplib
+from email.message import EmailMessage
 from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
+
+from rokdjangoprojekt.settings import EMAIL_HOST, EMAIL_HOST_PASSWORD, EMAIL_HOST_USER, EMAIL_PORT
 
 from .models import Ucitelj
 from django.contrib.auth.models import User
@@ -37,15 +40,28 @@ def send_registration_emails(modeladmin, request, queryset):
         registration_link = f"https://django.fbeu.eu/welcome?email={user.email}"
 
         # Poslji e-mail
-        send_mail(
-            subject="Registracija vasega racuna",
-            message=f"Pozdravljeni {user.first_name},\n\n"
-                    f"Prosimo obiscite {registration_link}, da vidite svoj racun.\n"
-                    f"Za nastavitev gesla, kliknite sem: {reset_link}\n\n"
-                    f"Hvala!",
-            from_email="admin@django.fbeu.eu",
-            recipient_list=[user.email],
-        )
+        username = EMAIL_HOST_USER
+        password = EMAIL_HOST_PASSWORD
+        msg = EmailMessage()
+        msg['Subject'] = "Registracija vasega racuna"
+        msg['From'] = username
+        msg['To'] = user.email
+        msg.set_content(f"Pozdravljeni {user.first_name},\n\n"
+                        f"Prosimo obiscite {registration_link}, da vidite svoj racun.\n"
+                        f"Za nastavitev gesla, kliknite sem: {reset_link}\n\n"
+                        f"Hvala!")
+        with smtplib.SMTP_SSL(EMAIL_HOST, EMAIL_PORT) as smtp:
+            smtp.login(username, password)
+            smtp.send_message(msg)
+        # send_mail(
+        #     subject="Registracija vasega racuna",
+        #     message=f"Pozdravljeni {user.first_name},\n\n"
+        #             f"Prosimo obiscite {registration_link}, da vidite svoj racun.\n"
+        #             f"Za nastavitev gesla, kliknite sem: {reset_link}\n\n"
+        #             f"Hvala!",
+        #     from_email="admin@django.fbeu.eu",
+        #     recipient_list=[user.email],
+        # )
 
 class UciteljAdmin(admin.ModelAdmin):
     list_display = ('ime', 'eposta')
